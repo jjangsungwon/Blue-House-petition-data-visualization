2dfeca6e521a24dc8ea7502a6a212b57a5c6fece
jjangsungwon <dnjs2113@gmail.com>
2020-06-02 02:16:41 +0900

    막대 그래프 시각화 완료

--- read_xlsx.py
+++ read_xlsx.py
@@ -1,6 +1,11 @@
 from openpyxl import load_workbook
 from konlpy.tag import Kkma
 import collections
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
+
 
 read_workbook = load_workbook("./bluehouse.xlsx")
 read_cell = read_workbook.active
@@ -25,5 +30,27 @@ for i in list_temp:
     else:
         result_list.append(i)
 
-print(collections.Counter(result_list))
-print(collections.Counter(result_list).common(5))  # 상위 5개
+#print(collections.Counter(result_list))
+#print(collections.Counter(result_list).most_common(5))  # 상위 5개
+result_list = dict(collections.Counter(result_list).most_common(20))  # 상위 20개
+
+# 시각화
+# 문자, 숫자 분리 (그래프를 그리기 위해서)
+list_string = []
+list_number = []
+for s, n in result_list.items():
+    list_string.append(s)
+    list_number.append(n)
+
+# 한글 깨짐 방지
+plt.rcParams["font.family"] = 'Malgun Gothic'
+
+label = list_string
+index = np.arange(len(label))
+plt.bar(index, list_number)
+plt.title("my title")
+plt.xlabel("x-label")
+plt.ylabel("y-label")
+plt.xticks(index, label)
+plt.xticks(rotation=60)
+plt.show()
